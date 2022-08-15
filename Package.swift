3102b450e5b6ee1acdca5c5a9916feebc13959e7
Nayanda Haberty <hainayanda@outlook.com>
2022-08-15 08:32:09 +0700

    Fix package swift

--- Package.swift
+++ Package.swift
@@ -23,9 +23,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Odeum",
-            dependencies: []),
-        .testTarget(
-            name: "OdeumTests",
-            dependencies: ["Odeum"]),
+            dependencies: []
+        ),
+//        .testTarget(
+//            name: "OdeumTests",
+//            dependencies: ["Odeum"]
+//        ),
     ]
 )
